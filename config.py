9b97d28b0a7948e90bbd2aa75ebf751f178dbc88
joevaas <133934323+joevaas@users.noreply.github.com>
2024-08-07 13:20:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Venkat3823:Venkat3823@cluster0.ig0oc9y.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "TAF_Luffy")
+DB_NAME = os.environ.get("DATABASE_NAME", "forkid")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1001628228663"))
